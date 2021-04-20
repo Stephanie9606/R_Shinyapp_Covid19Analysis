dc0c95863c860a011e72a29276a4b807aa9a7df3
Stephanie <stephaniehan628@gmail.com>
2021-04-20 20:53:33 +0800

    basic startup code

--- DS_Final_App/app.R
+++ DS_Final_App/app.R
@@ -1,49 +1,19 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# Author: Chiyun, Sihyuan, Trevor
+
+library(readr)
+library(tidyverse)
+library(ggplot2)
+
+readr::read_csv("../../DS_final_data/COVID-19_Case.csv") %>% 
+    drop_na() ->
+    covid19_data
 
 library(shiny)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# UI
+ui <- fluidPage()
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+# Server
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-    })
-}
-
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Application
+shinyApp(ui, server)
