68359e462ff36b845b60f53ccc533e4227371ef4
Stephanie <stephaniehan628@gmail.com>
2021-04-23 22:46:43 +0800

    new tidy code in app

--- DS_Final_App/app.R
+++ DS_Final_App/app.R
@@ -7,6 +7,17 @@ library(ggplot2)
 readr::read_rds("data/tidy_covid19_case.rds") -> 
   covid19_data
 
+covid19_data %>% 
+  na_if("Missing") %>% 
+  na_if("Unknown") %>% 
+  mutate(case_month = ym(case_month)) %>% 
+  mutate(sex = as.factor(sex),
+         hosp_yn = as.factor(hosp_yn),
+         icu_yn = as.factor(icu_yn),
+         death_yn = as.factor(death_yn),
+         underlying_conditions_yn = as.factor(underlying_conditions_yn)) ->
+  covid19_tidy
+
 library(shiny)
 
 # UI
