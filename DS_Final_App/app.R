98022fe3b814c91ba18e283f4a12b6e1e0aacca6
Stephanie <stephaniehan628@gmail.com>
2021-04-24 13:52:15 +0800

    tidy age_group

--- DS_Final_App/app.R
+++ DS_Final_App/app.R
@@ -7,15 +7,18 @@ library(ggplot2)
 readr::read_rds("data/tidy_covid19_case.rds") -> 
   covid19_data
 
-covid19_data %>% 
+covid19 %>% 
   na_if("Missing") %>% 
   na_if("Unknown") %>% 
   mutate(case_month = ym(case_month)) %>% 
-  mutate(sex = as.factor(sex),
+  mutate(age_group = str_replace(age_group, "to", "-"),
+         age_group = str_replace(age_group, "years", "")) %>% 
+  mutate(age_group = as.factor(age_group),
+         sex = as.factor(sex),
          hosp_yn = as.factor(hosp_yn),
          icu_yn = as.factor(icu_yn),
          death_yn = as.factor(death_yn),
-         underlying_conditions_yn = as.factor(underlying_conditions_yn)) ->
+         underlying_conditions_yn = as.factor(underlying_conditions_yn))  ->
   covid19_tidy
 
 library(shiny)
